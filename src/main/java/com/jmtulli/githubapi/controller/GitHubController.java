296fe87a233aca19a3fc8457b9d0d4cbda88af9b
jmtulli <jm.tulli@gmail.com>
2020-10-24 01:53:57 -0300

    Mapping

--- src/main/java/com/jmtulli/githubapi/controller/GitHubController.java
+++ src/main/java/com/jmtulli/githubapi/controller/GitHubController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitHubController {
 
-  @GetMapping
+  @GetMapping(path = "/hello")
   public String Hello() {
     return "Hello";
   }
