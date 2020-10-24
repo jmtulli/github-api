189b35009eb39bfe676dfc86436bfcce41a586d0
jmtulli <jm.tulli@gmail.com>
2020-10-24 01:35:08 -0300

    First commit

---
+++ src/test/java/com/jmtulli/githubapi/GitHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmtulli.githubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
