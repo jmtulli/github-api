fca36b7af07e938f9aaa2500f029f1f044cf0fa3
jmtulli <jm.tulli@gmail.com>
2020-10-26 11:54:36 -0300

    RabbitMQ testing

--- src/main/java/com/jmtulli/githubapi/GitHub.java
+++ src/main/java/com/jmtulli/githubapi/GitHub.java
@@ -9,10 +9,16 @@ package com.jmtulli.githubapi;
 
 import static com.jmtulli.githubapi.util.ApplicationConstants.URL_GITHUB;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 import com.jmtulli.githubapi.data.FileCounters;
 import com.jmtulli.githubapi.web.GitRepository;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 public class GitHub {
 
@@ -26,11 +32,27 @@ public class GitHub {
     String gitUrl = URL_GITHUB + "/" + gitUser + "/" + gitRepository;
     System.out.println("url: " + gitUrl);
 
-    Map<String, FileCounters> map = new GitRepository(gitUrl).process();
+    Map<String, FileCounters> map = null;//new GitRepository(gitUrl).process();
 
     long endTimer = System.nanoTime();
 
     System.out.println("Time: " + (endTimer - startTimer));
+    
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("coyote.rmq.cloudamqp.com");
+    factory.setPort(1883);
+    factory.setUsername("qgkjffns:qgkjffns");
+    factory.setPassword("0aqoD3P-WBsvN9EPQLfJb-HBPtVt7wBa");
+    try {
+      Connection connection = factory.newConnection();
+      Channel channel = connection.createChannel();
+      channel.queueDeclare("teste", false,false,false,null);
+      channel.basicPublish("", "teste", null, "Hello".getBytes());
+      System.out.println("Msg enviada");
+    } catch (IOException | TimeoutException e) {
+      e.printStackTrace();
+    }
+    
 
     return map;
   }
